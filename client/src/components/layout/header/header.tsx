e16a608d3ef49db986ed8a8c605c0e0aa137a22b
JakubBilski1 <jakub.bilski@spoko.pl>
2024-01-21 21:15:09 +0100

    copied project from previous repo + layout change

---
+++ client/src/components/layout/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="h-56" style={{backgroundColor: "#D89340"}}>
+      <img src="/cheetahbooking-high-resolution-logo.png" alt="logo" className="w-56"/>
+    </header>
+  )
+}
+
+export default Header
