0c0a13dbc5e9cbf9606cd74ac7dbd27378d5a5f5
JakubBilski1 <jakub.bilski@spoko.pl>
2024-01-25 21:04:15 +0100

    slight corrections

--- client/src/components/layout/header/header.tsx
+++ client/src/components/layout/header/header.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-md" style={{ backgroundColor: "#D89340" }}>
-      <nav className="flex-1 flex justify-between items-center">
-        <a href="/">
+    <header className="lg:px-16 flex py-4 shadow-md justify-between" style={{ backgroundColor: "#D89340" }}>
+      <a href="/">
           <img src="/cheetahbooking-high-resolution-logo.png" alt="logo" className="w-56" />
-        </a>
-
+      </a>
+      <nav className="flex-1 flex justify-end">
         <div className="hidden md:flex md:items-center md:w-auto w-full" id="menu">
-          <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-            <li><a className="md:p-4 py-3 px-0 block" href="#destinations">Destinations</a></li>
-            <li><a className="md:p-4 py-3 px-0 block" href="#pln">PLN</a></li>
-            <li><a className="md:p-4 py-3 px-0 block" href="#account">Konto</a></li>
+          <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0 gap-12">
+            <li><a className="block" href="/destinations">Destinations</a></li>
+            <li><button className="block">PLN</button></li>
+            <li><a className="block" href="/dashboard">Konto</a></li>
           </ul>
         </div>
       </nav>
@@ -21,4 +20,3 @@ function Header() {
 }
 
 export default Header;
-
