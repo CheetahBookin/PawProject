f36d459b7fb0f629ccd52bc605d2cd70f0a284e7
AKopydlowski <olekkopy@gmail.com>
2024-01-25 18:35:38 +0100

    header updated

--- client/src/components/layout/header/header.tsx
+++ client/src/components/layout/header/header.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
   return (
-    <header className="h-56" style={{backgroundColor: "#D89340"}}>
-      <img src="/cheetahbooking-high-resolution-logo.png" alt="logo" className="w-56"/>
+    <header className="lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-md" style={{ backgroundColor: "#D89340" }}>
+      <nav className="flex-1 flex justify-between items-center">
+        <a href="/">
+          <img src="/cheetahbooking-high-resolution-logo.png" alt="logo" className="w-56" />
+        </a>
+
+        <div className="hidden md:flex md:items-center md:w-auto w-full" id="menu">
+          <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+            <li><a className="md:p-4 py-3 px-0 block" href="#destinations">Destinations</a></li>
+            <li><a className="md:p-4 py-3 px-0 block" href="#pln">PLN</a></li>
+            <li><a className="md:p-4 py-3 px-0 block" href="#account">Konto</a></li>
+          </ul>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
