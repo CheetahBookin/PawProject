431100ac23011ad52cd8a694e35f6d37d52d1e04
SzKolodziej <szymonkolo555@gmail.com>
2024-02-03 23:51:50 +0100

    renaming + some changes

--- client/src/components/layout/searchBar/index.tsx
+++ client/src/components/layout/searchBar/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 
 function Search() {
@@ -16,7 +18,7 @@ function Search() {
   // };
 
   return (
-    <section className="flex flex-col items-center justify-center h-96 bg-brand-secondary  border-gray-300 rounded-md">
+    <section className="flex flex-col items-center justify-center bg-brand-secondary  border-gray-300 rounded-md">
       <h2 className="text-2xl font-bold mb-4">Wyszukiwarka</h2>
       <div className="flex space-x-2 mb-4">
         <input
