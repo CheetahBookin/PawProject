22898397e5facd959ff03b30893170b9fcc90f98
SzKolodziej <szymonkolo555@gmail.com>
2024-02-03 16:10:43 +0100

    change 'a' to 'Link'

--- client/src/components/layout/header/header.tsx
+++ client/src/components/layout/header/header.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="lg:px-16 flex py-4 shadow-md justify-between" style={{ backgroundColor: "#D89340" }}>
-      <a href="/">
+      <Link href="/">
           <img src="/cheetahbooking-high-resolution-logo.png" alt="logo" className="w-56" />
-      </a>
+      </Link>
       <nav className="flex-1 flex justify-end">
         <div className="hidden md:flex md:items-center md:w-auto w-full" id="menu">
           <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0 gap-12">
-            <li><a className="block" href="/destinations">Destinations</a></li>
+            <li><Link className="block" href="/destinations">Destinations</Link></li>
             <li><button className="block">PLN</button></li>
-            <li><a className="block" href="/dashboard">Konto</a></li>
+            <li><Link className="block" href="/dashboard">Konto</Link></li>
           </ul>
         </div>
       </nav>
