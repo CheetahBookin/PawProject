9f877713a333849460c7ba9018f3ee99cb356005
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-03 22:52:58 +0100

    new functions in hotel controller

--- server/src/controllers/hotel-controller.ts
+++ server/src/controllers/hotel-controller.ts
@@ -2,11 +2,73 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-interface HotelRequest extends Request {
-  body: {
-    name: string;
-    location: string;
-    price: number;
-  };
+const getHotels = async (req: Request, res: Response) => {
+  const hotels = await prisma.accomodation.findMany();
+  res.json(hotels);
 }
 
+const getExactHotel = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const hotel = await prisma.accomodation.findUnique({
+    where: {
+      id: parseInt(id)
+    },
+    include: {
+      images: { select: { image: true } },
+      Rooms: { select: { roomNumber: true, peopleCapacity: true, priceForPerson: true, childrenPrice: true } },
+      Rates: {
+        select: {
+          rate: true,
+          message: true,
+          date: true,
+          user: {
+            select: {
+              username: true
+            }
+          }
+        }
+      }
+    }
+  });
+  res.json(hotel);
+}
+
+
+const getDiscountedRooms = async (req: Request, res: Response) => {
+  const hotels = await prisma.accomodation.findMany({
+    where: {
+      Rooms: {
+        some: {
+          discount: {
+            gt: 0
+          }
+        }
+      }
+    },
+    select: {
+      name: true,
+      country: true,
+      city: true,
+      images: {
+        select: {
+          image: true
+        },
+        take: 1
+      },
+      Rooms: {
+        select: {
+          roomNumber: true,
+          priceForPerson: true,
+          discount: true
+        },
+        orderBy: {
+          discount: 'desc'
+        },
+        take: 1
+      }
+    },
+  });
+  res.json(hotels);
+}
+
+export { getHotels, getExactHotel, getDiscountedRooms };
