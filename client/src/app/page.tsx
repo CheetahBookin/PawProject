ea98ea39da937deb9d2b5f15400f36eaca0e6906
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-03 23:23:34 +0100

    some new styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
-import Search from '../components/layout/main/search'
+import Search from '../components/layout/searchBar/index'
 
 export default function Home() {
   return (
-    <main className="h-full text-gray-900 bg-brand-secondary">
+    <main className="h-full text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
        <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
        <Search/>
     </main>
