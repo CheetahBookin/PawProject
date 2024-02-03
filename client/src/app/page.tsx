d0bd3c7cd11e4b8c590dad0fbd004cf80c344c8d
AKopydlowski <olekkopy@gmail.com>
2024-02-03 18:21:20 +0100

    Page updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Search from '../components/layout/main/search'
 
 export default function Home() {
   return (
     <main className="h-full text-gray-900 bg-brand-secondary">
-        main
+       <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
+       <Search/>
     </main>
   )
 }
