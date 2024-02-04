cda1e34f45b65d25d27816b75c924fc46346de01
SzKolodziej <szymonkolo555@gmail.com>
2024-02-04 23:17:35 +0100

    created register page

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,40 +1,23 @@
 import React from 'react'
-import Link from "next/link";
+import Link from 'next/link'
+import Label from '@/components/common/label'
+import PasswordInput from '@/components/common/passwordInput'
+
 
 function RegisterSite(){
     return(
-        <section className='flex flex-col items-center'>
-            <h1 className='text-4xl font-bold mt-3'>Sign up</h1>
-            <form className='mt-5 w-64 flex flex-col items-center border border-gray-300 shadow-md p-6 rounded-lg'>
-                <label className='block'>
-                    <span className='text-gray-700'>First Name</span>
-                    <input type='text'
-                           className='mt-1 block w-full border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 '/>
-                </label>
-                <label>
-                    <span>Last Name</span>
-                    <input type='text'/>
-                </label>
-                <label>
-                    Address
-                    <input type='text'/>
-                </label>
-                <label>
-                    User name
-                    <input type='text'/>
-                </label>
-                <label>
-                    Password
-                    <input type='password'/>
-                </label>
-                <label>
-                    Confirm Password
-                    <input type='password'/>
-                </label>
+        <section className='flex flex-col items-center h-full justify-center'>
+            <h1 className='text-4xl font-bold'>Sign up</h1>
+            <form className='mt-5 w-80 flex flex-col items-center border border-gray-300 shadow-md p-6 rounded-lg'>
+                <Label inputType={'text'} spanContent={'First name'}/>
+                <Label inputType={'text'} spanContent={'Last name'}/>
+                <Label inputType={'text'} spanContent={'Address'}/>
+                <Label inputType={'text'} spanContent={'Username'}/>
+                <PasswordInput/>
 
-                <button>Sign up</button>
+                <button className='mt-2 bg-brand-primary hover:bg-brand-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus::shadow-outline'>Sign up</button>
             </form>
-            <p>Already have an account? <Link href='/login'>Login here</Link></p>
+            <p className='mt-4 text-sm'>Already have an account? <Link href='/login' className='text-brand-primary font-bold'>Login here</Link></p>
         </section>
     )
 }
