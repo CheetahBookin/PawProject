0176c159e05ae4ba3645a26694e944b59de01baf
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-05 00:50:27 +0100

    login site finished

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="h-full text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
        <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
-       <Search/>
+       <Search />
     </main>
   )
 }
