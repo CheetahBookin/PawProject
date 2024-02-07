2cadd30e9b9e75396f141e2b3fd605c1ba5e3aa9
AKopydlowski <olekkopy@gmail.com>
2024-02-07 17:51:27 +0100

    updated searchBar

--- client/src/components/layout/searchBar/index.tsx
+++ client/src/components/layout/searchBar/index.tsx
@@ -3,19 +3,19 @@
 import React, { useState } from 'react';
 
 function Search() {
-  // const [destination, setDestination] = useState('');
-  // const [checkInDate, setCheckInDate] = useState('');
-  // const [checkOutDate, setCheckOutDate] = useState('');
-  // const [numberOfPersons, setNumberOfPersons] = useState('');
+  const [destination, setDestination] = useState('');
+  const [checkInDate, setCheckInDate] = useState('');
+  const [checkOutDate, setCheckOutDate] = useState('');
+  const [numberOfPersons, setNumberOfPersons] = useState('');
 
-  // const handleSearch = () => {
-  //   console.log('Wyszukiwanie dla:', {
-  //     destination,
-  //     checkInDate,
-  //     checkOutDate,
-  //     numberOfPersons,
-  //   });
-  // };
+  const handleSearch = () => {
+    console.log('Wyszukiwanie dla:', {
+      destination,
+      checkInDate,
+      checkOutDate,
+      numberOfPersons,
+    });
+  };
 
   return (
     <section className="flex flex-col items-center justify-center bg-brand-secondary  border-gray-300 rounded-md">
@@ -26,24 +26,24 @@ function Search() {
           id="destination"
           className="w-1/3 h-10 px-3 border border-gray-300 rounded-l-md focus:outline-none"
           placeholder="Dokąd się wybierasz?"
-          // value={destination}
-          // onChange={(e) => setDestination(e.target.value)}
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
         />
         <input
           type="date"
           id="dateCheckIn"
           className="w-1/4 h-10 px-3 border border-gray-300 focus:outline-none"
           placeholder="Od"
-          // value={checkInDate}
-          // onChange={(e) => setCheckInDate(e.target.value)}
+          value={checkInDate}
+          onChange={(e) => setCheckInDate(e.target.value)}
         />
         <input
           type="date"
           id="dateCheckOut"
           className="w-1/4 h-10 px-3 border border-gray-300 focus:outline-none"
           placeholder="Do"
-          // value={checkOutDate}
-          // onChange={(e) => setCheckOutDate(e.target.value)}
+          value={checkOutDate}
+          onChange={(e) => setCheckOutDate(e.target.value)}
         />
         <input
           type="number"
@@ -51,13 +51,13 @@ function Search() {
           className="w-1/4 h-10 px-3 border border-gray-300 focus:outline-none"
           placeholder="Liczba osób"
           min="1"
-          // value={numberOfPersons}
-          // onChange={(e) => setNumberOfPersons(e.target.value)}
+          value={numberOfPersons}
+          onChange={(e) => setNumberOfPersons(e.target.value)}
         />
         <button
         id="search"
         className="h-10 bg-brand-primary text-white rounded-md px-4"
-        // onClick={handleSearch}
+        onClick={handleSearch}
       >
         Szukaj
       </button>
