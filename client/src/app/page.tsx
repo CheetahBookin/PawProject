88e71b51521584520f67f16629873d3c3be97c42
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-07 20:18:00 +0100

    little change in styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,7 +7,7 @@ import GalleryPropertyType from '@/components/layout/galleryPropertyType'
 
 export default function Home() {
   return (
-    <main className="h-full text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
+    <main className="text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
        <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
        <Search />
        <GalleryPopularDestinations />
