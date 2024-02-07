d9535b3a320b106e09155d7e4be033882dce00b8
AKopydlowski <olekkopy@gmail.com>
2024-02-07 18:23:14 +0100

    Updated page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,15 @@
+import GalleryPopularDestinations from '@/components/layout/galleryPopularDestinations'
 import GallerySlider1 from '@/components/layout/gallerySlider1'
 import GallerySlider2 from '@/components/layout/gallerySlider2'
 import Search from '../components/layout/searchBar/index'
+
 
 export default function Home() {
   return (
     <main className="h-full text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
        <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
        <Search />
+       <GalleryPopularDestinations />
        <GallerySlider1 />
        <GallerySlider2 />
     </main>
