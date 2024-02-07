05d7d2c3cce1432e5adc76ad000f0229c01f4295
AKopydlowski <olekkopy@gmail.com>
2024-02-07 17:47:10 +0100

    page.tsx updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import GallerySlider1 from '@/components/layout/gallerySlider1'
 import Search from '../components/layout/searchBar/index'
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="h-full text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
        <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
        <Search />
+       <GallerySlider1 />
     </main>
   )
 }
