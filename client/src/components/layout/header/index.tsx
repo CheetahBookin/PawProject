b50335e6b95571ac19887b1574a12a9e2ff7e065
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-11 21:38:21 +0100

    new renders on header

--- client/src/components/layout/header/index.tsx
+++ client/src/components/layout/header/index.tsx
@@ -1,8 +1,39 @@
+"use client"
+
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { getUser } from "@/services/userService";
+import { User } from "@/types/userTypes";
+import { useUserContext } from "@/Context/userContext";
+import Loading from "@/components/common/loading";
 
 function Header() {
+  const { isLogged, setIsLogged } = useUserContext();
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        setLoading(true);
+        const userData = await getUser();
+        if(userData.status === 200){
+          setUser(userData.data)
+          setIsLogged(true)
+        }else{
+          setIsLogged(false)
+        }
+      } catch (error: any) {
+        console.log(error)
+      }finally{
+        setLoading(false)
+      }
+    };
+    fetchUser();
+  }, [isLogged]);
+
   const currency = [
     { value: "PLN", label: "PLN" },
     { value: "USD", label: "USD" },
@@ -14,7 +45,8 @@ function Header() {
     { value: "CNY", label: "CNY" },
     { value: "KRW", label: "KRW" },
     { value: "SGD", label: "SGD" }
-  ]
+  ];
+
   return (
     <header className="lg:px-16 flex py-4 shadow-md justify-between bg-brand-primary">
       <Link href="/">
@@ -23,6 +55,7 @@ function Header() {
       <nav className="flex-1 flex justify-end">
         <div className="hidden md:flex md:items-center md:w-auto w-full" id="menu">
           <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0 gap-12">
+            {isLogged && <li>{!loading ? <p>Welcome on your adventure {user?.username}!</p> : <Loading />}</li>}
             <li><Link className="block" href="/destinations">Destinations</Link></li>
             <li>
               <select className="appearance-none bg-brand-secondary border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
@@ -31,7 +64,15 @@ function Header() {
                 ))}
               </select>
             </li>
-            <li><Link className="block" href="/login"><FontAwesomeIcon icon={faUser} className="w-6"/></Link></li>
+            {!loading ? <li>{!isLogged ? <Link className="block" href="/login"><FontAwesomeIcon icon={faUser} className="w-12 h-6"/></Link> : 
+              <Link className="block" href="/dashboard">
+                <img
+                  src="https://placehold.it/50x50"
+                  alt="Profile"
+                  className="w-full h-full object-cover border-r-2 border-brand-secondary rounded-full"
+                />
+              </Link>}
+            </li> : <Loading />}
           </ul>
         </div>
       </nav>
