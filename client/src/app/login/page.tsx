ed1e30bd2e8ddb2f992eacb06281b8320cd399d2
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-11 21:36:45 +0100

    error handling + context in login

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import { useContext } from 'react'
 import Link from 'next/link'
 import Label from '../../components/common/label'
 import PasswordInput from '@/components/common/passwordInput'
@@ -7,8 +7,13 @@ import { LoginData } from '@/types/authTypes'
 import { login } from '@/services/authService'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { useUserContext } from '@/Context/userContext'
+import Loading from '@/components/common/loading'
 
 function Login(){
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
+    const { setIsLogged } = useUserContext()
     const [data, setData] = useState<LoginData>({
         email: '',
         password: ''
@@ -26,13 +31,19 @@ function Login(){
 
     const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
         e.preventDefault()
+        setLoading(true)
         try{
             const response = await login(data.email, data.password)
             if(response.status === 200){
+                setIsLogged(true)
                 router.push('/')
+            }else{
+                setError(response.data.error)
             }
         }catch(err: any){
-            console.log(err.response.data.error)
+            console.log(err)
+        }finally{
+            setLoading(false)
         }
     }
     return(
@@ -46,7 +57,8 @@ function Login(){
                     <p className='text-sm text-gray-600 inline self-end cursor-pointer'>Forgot password?</p>
                 </div>
 
-                <button className='mt-2 bg-brand-secondary hover:bg-brand-primary transition-all text-gray-600 font-bold py-2 px-4 rounded focus:outline-none focus::shadow-outline' onClick={e=>handleSubmit(e)}>Login</button>
+                <button className='mt-2 bg-brand-secondary hover:bg-brand-primary transition-all text-gray-600 font-bold py-2 px-4 rounded focus:outline-none focus::shadow-outline' onClick={e=>handleSubmit(e)}>{loading ? <Loading /> : "Login"}</button>
+                {error && <p className='text-red-500 text-sm mt-2'>{error}</p>}
             </form>
 
             <p className='mt-4 text-sm text-gray-600'>Don't have an account? <Link href='/register' className='text-brand-primary font-bold'>Sign up</Link></p>
