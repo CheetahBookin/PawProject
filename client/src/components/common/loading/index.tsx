c4380e2ea737ac12554ed2bbe50a23802678831d
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-11 21:38:05 +0100

    new loading component

---
+++ client/src/components/common/loading/index.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="border-4 border-gray-200 border-t-4 border-t-gray-800 rounded-full h-8 w-8 animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
