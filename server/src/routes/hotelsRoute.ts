becda047355e804b7a38a098dfcafb8372df01b1
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-12 01:38:57 +0100

    new hotel routes

--- server/src/routes/hotelsRoute.ts
+++ server/src/routes/hotelsRoute.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
-import { getHotels, getExactHotel, getDiscountedRooms } from "../controllers/hotel-controller";
+import { getHotels, getExactHotel, getDiscountedRooms, getHotelsTypes, browseByPropertyType } from "../controllers/hotel-controller";
 
 const router: Router = express.Router();
 
 router.get("/", getHotels);
 router.get("/:id", getExactHotel);
 router.get("/r/discount", getDiscountedRooms);
+router.get("/h/types", getHotelsTypes);
+router.post("/h/types", browseByPropertyType);
 
 export default router;
