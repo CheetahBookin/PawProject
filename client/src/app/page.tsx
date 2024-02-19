02704d823e845309a6183920d3049c918b5effaf
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-19 22:52:18 +0100

    removed motto

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,7 +8,6 @@ import GalleryPropertyType from '@/components/layout/galleryPropertyType'
 export default function Home() {
   return (
     <main className="text-gray-900 bg-brand-secondary flex flex-col items-center pt-4 gap-12">
-       <h1>Book. Stay. Play. Your Adventure Awaits!</h1>
        <Search />
        <GalleryPopularDestinations />
        <GalleryPropertyType />
