53fee7677c22319baaea3fa92cfef252320ba93e
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-19 22:52:41 +0100

    added terms and conditions downloadable

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -60,7 +60,7 @@ function Register(){
             <form className='mt-5 w-80 flex flex-col items-center border border-gray-300 shadow-md p-6 rounded-lg bg-brand-primary'>
                 <Label inputType={'email'} spanContent={'Email'} onChange={e=>handleChange(e)} name={'email'} className='mt-1 block w-full border-gray-300 rounded-md py-2 px-3 focus:outline-none text-gray-600'/>
                 <Label inputType={'text'} spanContent={'Username'} onChange={e=>handleChange(e)} name={'username'} className='mt-1 block w-full border-gray-300 rounded-md py-2 px-3 focus:outline-none text-gray-600'/>
-                <LabelCheckbox content='I agree to the terms and conditions' onChange={e=>handleChange(e)} important={true} name={'agreement'}/>
+                <LabelCheckbox content='' onChange={e=>handleChange(e)} important={true} name={'agreement'} terms={true}/>
                 <PasswordInput content={'Password'} onChange={e=>handleChange(e)} name={'password'}/>
                 <PasswordInput content={'Confirm Password'} onChange={e=>handleChange(e)} name={'confirmPassword'}/>
                 <button className='mt-2 bg-brand-secondary hover:bg-brand-primary transition-all text-gray-600 font-bold py-2 px-4 rounded focus:outline-none focus::shadow-outline' onClick={e=>handleSubmit(e)}>{loading ? <Loading /> : "Sign up"}</button>
--- client/src/components/common/labelCheckbox/index.tsx
+++ client/src/components/common/labelCheckbox/index.tsx
@@ -1,17 +1,31 @@
 interface LabelCheckboxProps {
-    content: string
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-    important?: boolean
-    name: string
+  content: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  important?: boolean;
+  name: string;
+  terms?: boolean;
 }
 
-function LabelCheckbox({content, onChange, important, name}: LabelCheckboxProps) {
+function LabelCheckbox({ content, onChange, important, name, terms = false }: LabelCheckboxProps) {
   return (
     <label>
-      <input type='checkbox' onChange={onChange} name={name}/>
-      <span className={important ? 'text-gray-700 pl-1' : 'text-sm text-gray-700 pl-1'}>{content}</span>
+      <input type='checkbox' onChange={onChange} name={name} />
+      <span className={important ? 'text-gray-700 pl-1' : 'text-sm text-gray-700 pl-1'}>
+        {!terms ? content : (
+          <span>
+            I agree to the{' '}
+            <a
+              href="/assets/CheetahBooking_TermsAndConditions.pdf"
+              download="CheetahBooking_TermsAndConditions.pdf"
+              className="hover:text-gray-500 transition-all underline"
+            >
+              terms and conditions
+            </a>
+          </span>
+        )}
+      </span>
     </label>
-  )
+  );
 }
 
-export default LabelCheckbox
+export default LabelCheckbox;
