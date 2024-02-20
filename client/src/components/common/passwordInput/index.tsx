e6313859141a78e447960ead8958034ac722a832
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-20 23:18:19 +0100

    Changed rendering of showing password

--- client/src/components/common/passwordInput/index.tsx
+++ client/src/components/common/passwordInput/index.tsx
@@ -1,33 +1,39 @@
 'use client'
-import Label from '@/components/common/label'
-import {useState} from 'react'
-import LabelCheckbox from '../labelCheckbox'
 
-interface PasswordInputProps{
-    content: string
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-    name: string
+import Label from '@/components/common/label';
+import { useState } from 'react';
+
+interface PasswordInputProps {
+    content: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    name: string;
 }
 
-function PasswordInput({content, onChange, name}: PasswordInputProps){
-    const [checked, setChecked] = useState(false)
+function PasswordInput({ content, onChange, name }: PasswordInputProps) {
+    const [showPassword, setShowPassword] = useState(false);
 
-    const togglePasswordVisibility = ()=>{
-        if(checked){
-            setChecked(false)
-        }
-        else{
-            setChecked(true)
-        }
-    }
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
-
-    return(
-        <div className='flex flex-col w-full'>
-            <Label inputType={checked ? 'text' : 'password'} spanContent={content} onChange={onChange} name={name} className='mt-1 block w-full border-gray-300 rounded-md py-2 px-3 focus:outline-none text-gray-600'/>
-            <LabelCheckbox content='Show password' onChange={togglePasswordVisibility} name={name}/>
+    return (
+        <div className='flex flex-col w-full relative'>
+            <Label
+                inputType={showPassword ? 'text' : 'password'}
+                spanContent={content}
+                onChange={onChange}
+                name={name}
+                className='mt-1 block w-full border-gray-300 rounded-md py-2 px-3 focus:outline-none text-gray-600 pr-12'
+            />
+            <button
+                type="button"
+                className="absolute inset-y-12 right-0 flex items-center pr-3 text-sm text-gray-600 cursor-pointer"
+                onClick={togglePasswordVisibility}
+            >
+                {showPassword ? 'Hide' : 'Show'}
+            </button>
         </div>
-    )
+    );
 }
 
-export default PasswordInput
+export default PasswordInput;
