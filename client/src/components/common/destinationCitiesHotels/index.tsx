77cccc65488f3b5ea29a9d3760fed5b928bb720f
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-21 02:19:11 +0100

    added redirection to hotel

--- client/src/components/common/destinationCitiesHotels/index.tsx
+++ client/src/components/common/destinationCitiesHotels/index.tsx
@@ -1,4 +1,6 @@
 import { browseHotelsByCity } from '@/services/countryService'
+import { HotelType } from '@/types/countryTypes'
+import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 interface DestinationsCitiesHotelsProps {
@@ -7,24 +9,35 @@ interface DestinationsCitiesHotelsProps {
 }
 
 function DestinationsCitiesHotels({ selectedCity, setSelectedCity }: DestinationsCitiesHotelsProps) {
-  const [hotels, setHotels] = useState([])
+  const [hotels, setHotels] = useState<HotelType[]>([])
   useEffect(() => {
     const fetchHotels = async () => {
       if(!selectedCity) return
       const response = await browseHotelsByCity(selectedCity)
-      setHotels(response.data)
+      setHotels(response.data as HotelType[])
     }
     fetchHotels()
   }, [selectedCity])
+
+  const router = useRouter();
+  const createSlug = (name: string, id: number) =>{
+    return `${name.toLowerCase().split(' ').join('-')}-${id}`;
+  }
+
+  const handleClick = (hotel: HotelType) =>{
+    const slug = createSlug(hotel.name, hotel.id)
+    router.push(`/hotel/${slug}`);
+  }
+
   return (
     <div className='p-4 w-full bg-gray-100'>
-        {hotels.map((hotel: any, index: number) => (
+        {hotels.map((hotel: HotelType, index: number) => (
             <div key={index} className="flex flex-col p-4 border-b border-gray-300 w-full">
                 <div className='flex justify-between text-3xl items-center'>
                     <h2 className="text-2xl font-semibold mb-2">Explore hotels in {selectedCity}</h2>
                     <button onClick={()=>setSelectedCity(null)} className='cursor-pointer'>x</button>
                 </div>
-                <div className='flex items-center cursor-pointer hover:bg-gray-300'>
+                <div className='flex items-center cursor-pointer hover:bg-gray-300' onClick={()=>handleClick(hotel)}>
                     <img src={hotel.images[0].image} alt={hotel.name} className="w-52 h-40  object-cover mr-4" />
                     <h3 className="text-lg font-semibold">{hotel.name}</h3>
                 </div>
