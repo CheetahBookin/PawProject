4d08a5a4648b74e704a8293c4e88dd1edc2e7e5b
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-24 23:46:09 +0100

    added socket in reservations component

--- client/src/app/dashboard/reservations/page.tsx
+++ client/src/app/dashboard/reservations/page.tsx
@@ -6,25 +6,28 @@ import React, { useEffect, useState } from 'react'
 import { useUserContext } from '@/context/userContext'
 import ReservationCard from '@/components/common/reservation'
 import { Reservation } from '@/types/paymentTypes'
+import { getSocket } from '@/services/getSocket'
 
 function Reservations() {
-  const [reservations, setReservations] = useState<Reservation[]>([])
+  const [reservation, setReservation] = useState<number | null>(null)
   const [paidReservations, setPaidReservations] = useState<Reservation[]>([])
   const [unpaidReservations, setUnpaidReservations] = useState<Reservation[]>([])
   const [showPaidReservations, setShowPaidReservations] = useState<boolean>(false)
   const { setIsLogged } = useUserContext()
   const router = useRouter()
+  const socket = getSocket()
 
   useEffect(() => {
     const fetchUser = async () => {
       const user = await getUser()
       if(user.status === 200) {
         const allReservations = user.data.orders || [];
-        setReservations(allReservations);
-        const unpaid = allReservations.filter((reservation: Reservation) => !reservation.paid);
-        setUnpaidReservations(unpaid);
         const paid = allReservations.filter((reservation: Reservation) => reservation.paid);
         setPaidReservations(paid);
+        socket.on("reservations", (reservations: Reservation[]) => {
+          setUnpaidReservations(reservations);
+        })
+        socket.emit("get-reservations")
         setIsLogged(true)
       } else {
         setIsLogged(false)
@@ -32,22 +35,23 @@ function Reservations() {
       }
     }
     fetchUser()
-  }, [])
+  }, [socket])
 
   return (
     <main className="bg-brand-secondary text-gray-700 flex flex-col gap-12 h-auto pb-[14.5vh]">
-      {unpaidReservations.length > 0 &&
-        <>
-          <h1>Unpaid reservations:</h1>
-          <div className="flex flex-col items-center">
-            {unpaidReservations.map((reservation, index) => {
-              return (
-                <ReservationCard key={index} reservation={reservation} />
-              )
-            })}
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="text-4xl font-semibold text-center">Unpaid reservations</h1>
+        {unpaidReservations.length > 0 ? unpaidReservations.map((reservation, index) => {
+          return (
+            <ReservationCard key={index} reservation={reservation} />
+          )
+        }) : (
+          <div className="flex justify-center items-center h-20">
+            <p className="text-gray-500">No unpaid reservations</p>
           </div>
-        </>
-      }
+        )
+        }
+      </div>
       {paidReservations.length > 0 && (
         <div className="flex justify-center flex-col items-center">
           <button className="bg-brand-primary text-white px-4 py-2 rounded-md my-4" onClick={() => setShowPaidReservations(prevState => !prevState)}>
