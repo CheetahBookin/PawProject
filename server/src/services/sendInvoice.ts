a87effd5ebf25f5903495c1b3e5ae10c679ddc01
JakubBilski1 <jakub.bilski@spoko.pl>
2024-03-29 21:00:34 +0100

    fully renovated sendInvoice function (new emails and pdfs)

--- server/src/services/sendInvoice.ts
+++ server/src/services/sendInvoice.ts
@@ -1,332 +1,156 @@
-import PDFDocument, { text } from 'pdfkit';
-import fs from 'fs';
-import nodemailer from 'nodemailer';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import nodemailer from 'nodemailer'
+import jsPDf from 'jspdf'
+import path from 'path'
+import fs from 'fs'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-const sendInvoice = async (email: string, orderId: number) => {
-    try{
-        const order = await prisma.orders.findUnique({
-            where: {
-                id: orderId
-            }
-        });
-        if(!order){
-            return;
-        }
-        const user = await prisma.user.findUnique({
-            where: {
-                id: order.userId
-            }
-        });
-        if(!user){
-            return;
-        }
-        const hotel = await prisma.accomodation.findUnique({
-            where: {
-                id: order.hotelId
-            },
-            include: {
-                images: true
-            }
-        });
-        if(!hotel){
-            return;
-        }
-        const room = await prisma.rooms.findUnique({
-            where: {
-                id: order.roomId
-            }
-        });
-        if(!room){
-            return;
-        }
-        const invoice = await prisma.invoice.findFirst({
-            where: {
-                orderId: orderId
-            }
-        });
-        if(!invoice){
-            return;
-        }
-        const nights = Math.floor((new Date(order.toDate).getTime() - new Date(order.fromDate).getTime()) / (1000 * 60 * 60 * 24));
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: process.env.EMAIL,
-              pass: process.env.EMAIL_PASSWORD
-            }
-          });
-          const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: 'Cheetah Booking - Invoice',
-            html: `<h1>Invoice</h1>
-                <div style="display: flex; justify-content: space-between;">
-                    <img src="../client/public/cheetahbooking-high-resolution-logo.png" alt="Cheetah Logo" />
-                    <p>Mr./Mrs. ${invoice.name}</p>
-                    <p>${invoice.email}</p>
-                    <p>${invoice.address}</p>
-                    <p>${invoice.city}, ${invoice.postalCode}</p>
-                    <p>${invoice.country}</p>
-                </div>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>Reckoning</th>
-                        </tr>
-                        <tr>
-                            <td>Order Id:</td>
-                            <td>${invoice.orderId}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Hotel</th>
-                    </tr>
-                    <tr>
-                        <td>Name:</td>
-                        <td>${hotel.name}</td>
-                    </tr>
-                    <tr>
-                        <td>Address:</td>
-                        <td>${hotel.address}, ${hotel.city}, ${hotel.country}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Room</th>
-                    </tr>
-                    <tr>
-                        <td>Name:</td>
-                        <td>${room.roomNumber}</td>
-                    </tr>
-                    <tr>
-                        <td>Adult price: </td>
-                        <td>${order.adults}*${room.priceForPerson} | ${order.adults * room.priceForPerson}</td>
-                    </tr>
-                    <tr>
-                        <td>Children price: </td>
-                        <td>${order.children}*${room.childrenPrice} | ${order.children * room.childrenPrice}</td>
-                    </tr>
-                    <tr>
-                        <td>Discount: </td>
-                        <td>${room.discount ? room.discount * 100 : `0`}%</td>
-                    </tr>
-                    <tr>
-                        <td>Parking fee: </td>
-                        <td>${order.carParkFee}*${nights} | ${order.carParkFee * nights}</td>
-                    </tr>
-                    <tr>
-                        <td>Total price: </td>
-                        <td>${order.fullPrice}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Dates</th>
-                    </tr>
-                    <tr>
-                        <td>Check-in:</td>
-                        <td>${order.fromDate}, 12:00</td>
-                    </tr>
-                    <tr>
-                        <td>Check-out:</td>
-                        <td>${order.toDate} 10:00</td>
-                    </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Payment</th>
-                    </tr>
-                    <tr>
-                        <td>Status:</td>
-                        <td>Paid</td>
-                    </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>People count</th>
-                    </tr>
-                    <tr>
-                        <td>Adults:</td>
-                        <td>${order.adults}</td>
-                    </tr>
-                    <tr>
-                        <td>Children:</td>
-                        <td>${order.children}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <p>Thank you for choosing Cheetah Booking</p>`
-        };    
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-        });
-        const doc = new PDFDocument();
-        doc.pipe(fs.createWriteStream(`../client/public/invoice/invoice_${orderId}.pdf`));
-
-        // Funkcja sprawdzająca i ewentualnie przenosząca do nowej strony
-        function checkAndAdvancePage(textHeight: number) {
-            if (textY + textHeight > doc.page.height - 50) {
-                doc.addPage();
-                textY = 50;
-            }
-        }
-
-        // Ustawienia zdjęcia
-        const imageWidth = 300;
-        const imageHeight = 250;
-        const imageX = (doc.page.width - imageWidth) / 2;
-        const imageY = 50;
-        doc.image('../client/public/cheetahbooking-high-resolution-logo.png', imageX, imageY, { width: imageWidth, height: imageHeight });
-
-        // Początkowa pozycja tekstu
-        let textY = imageY + imageHeight + 50;
-
-        // Nagłówek
-        doc.fontSize(25).text('Invoice', 100, textY);
-        textY += 50;
-
-        doc.moveTo(100, textY).lineTo(500, textY).stroke();
-        textY += 20;
-
-        // Dane osobowe
-        doc.fontSize(15).text(`Mr./Mrs. ${invoice.name}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`${invoice.email}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`${invoice.address}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`${invoice.city}, ${invoice.postalCode}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`${invoice.country}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        doc.moveTo(100, textY).lineTo(500, textY).stroke();
-        textY += 20;
-
-        // Rozliczenie
-        doc.fontSize(15).text('Reckoning', 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Order id: ${invoice.orderId}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        // Hotel
-        doc.fontSize(15).text('Hotel', 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Name: ${hotel.name}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Address: ${hotel.address}, ${hotel.city}, ${hotel.country}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        // Pokój
-        doc.fontSize(15).text('Room', 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Name: ${room.roomNumber}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        const adultPrice = order.adults * room.priceForPerson;
-        doc.fontSize(15).text(`Adult price: ${order.adults}*${room.priceForPerson} | ${adultPrice}PLN`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        const childrenPrice = order.children * room.childrenPrice;
-        doc.fontSize(15).text(`Children price: ${order.children}*${room.childrenPrice} | ${childrenPrice}PLN`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`All nights: (${adultPrice + childrenPrice})*${nights} | ${(adultPrice+childrenPrice)*nights}PLN`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        room.discount ? doc.fontSize(15).text(`Discount: ${room.discount * 100}%`, 100, textY) : doc.fontSize(15).text(`Discount: 0%`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Parking fee: ${order.carParkFee}*${nights} | ${order.carParkFee * nights}PLN`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        doc.moveTo(100, textY).lineTo(300, textY).stroke();
-        doc.fontSize(15).text(`Total price: ${order.fullPrice}PLN`, 100, textY + 10);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        doc.moveTo(100, textY).lineTo(500, textY).stroke();
-        textY += 20;
-
-        // Daty
-        doc.fontSize(15).text('Dates', 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Check-in: ${order.fromDate.toISOString().split('T')[0]}, 12:00`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Check-out: ${order.toDate.toISOString().split('T')[0]}, 10:00`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        doc.moveTo(100, textY).lineTo(500, textY).stroke();
-        textY += 20;
-
-        // Płatność
-        doc.fontSize(15).text('Payment', 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Status: Paid`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        doc.moveTo(100, textY).lineTo(500, textY).stroke();
-        textY += 20;
-
-        // Osoby
-        doc.fontSize(15).text('People', 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Adults: ${order.adults}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-        doc.fontSize(15).text(`Children: ${order.children}`, 100, textY);
-        textY += 30;
-        checkAndAdvancePage(30);
-
-        doc.moveTo(100, textY).lineTo(500, textY).stroke();
-        textY += 20;
-
-        doc.fontSize(15).text('Thank you for choosing Cheetah Booking!', 100, textY);
-
-        doc.end();
-
-
-        console.log('invoice sent')
-        return "Invoice sent";
-    }catch(error){
-        console.log(error);
-        return error;
+const sendInvoice = async (email: string, orderId: string) => {
+  try {
+    const order = await prisma.orders.findUnique({
+      where: {
+        id: orderId,
+      },
+    })
+    if (!order) {
+      return
     }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: order.userId,
+      },
+    })
+    if (!user) {
+      return
+    }
+    const hotel = await prisma.accomodation.findUnique({
+      where: {
+        id: order.hotelId,
+      },
+      include: {
+        images: true,
+      },
+    })
+    if (!hotel) {
+      return
+    }
+    const room = await prisma.rooms.findUnique({
+      where: {
+        id: order.roomId,
+      },
+    })
+    if (!room) {
+      return
+    }
+    const invoice = await prisma.invoice.findFirst({
+      where: {
+        orderId: orderId,
+      },
+    })
+    if (!invoice) {
+      return
+    }
+    const nights = Math.floor(
+      (new Date(order.toDate).getTime() - new Date(order.fromDate).getTime()) /
+        (1000 * 60 * 60 * 24),
+    )
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
+    if(!invoice.name || !invoice.email || !invoice.address || !invoice.city || !invoice.postalCode || !invoice.country){
+      return 'Invoice not found'
+    }
+    const templatePath = path.join(__dirname, 'emailTemplate.html')
+    let emailTemplate = fs.readFileSync(templatePath, 'utf8')
+    const logoPath = path.join(__dirname, '..', '..', '..', 'client', 'public', 'cheetahbooking-high-resolution-logo.png')
+    emailTemplate = emailTemplate.replace('{{logo}}', logoPath)
+    emailTemplate = emailTemplate.replace('{{name}}', invoice.name)
+    emailTemplate = emailTemplate.replace('{{email}}', invoice.email)
+    emailTemplate = emailTemplate.replace('{{address}}', invoice.address)
+    emailTemplate = emailTemplate.replace('{{city}}', invoice.city)
+    emailTemplate = emailTemplate.replace('{{postalCode}}', invoice.postalCode)
+    emailTemplate = emailTemplate.replace('{{country}}', invoice.country)
+    emailTemplate = emailTemplate.replace('{{orderId}}', orderId)
+    emailTemplate = emailTemplate.replace('{{hotelName}}', hotel.name)
+    emailTemplate = emailTemplate.replace('{{hotelAddress}}', hotel.address)
+    emailTemplate = emailTemplate.replace('{{hotelCity}}', hotel.city)
+    emailTemplate = emailTemplate.replace('{{hotelCountry}}', hotel.country)
+    emailTemplate = emailTemplate.replace('{{roomNumber}}', room.roomNumber)
+    emailTemplate = emailTemplate.replace('{{adults}}', order.adults.toString())
+    emailTemplate = emailTemplate.replace('{{priceForPerson}}', room.priceForPerson.toString())
+    emailTemplate = emailTemplate.replace('{{adultPrice}}', (order.adults * room.priceForPerson).toString())
+    emailTemplate = emailTemplate.replace('{{children}}', order.children.toString())
+    emailTemplate = emailTemplate.replace('{{childrenPrice}}', room.childrenPrice.toString())
+    emailTemplate = emailTemplate.replace('{{finalChildrenPrice}}', (order.children * room.childrenPrice).toString())
+    emailTemplate = emailTemplate.replace('{{nights}}', nights.toString())
+    emailTemplate = emailTemplate.replace('{{carParkFee}}', order.carParkFee.toString())
+    emailTemplate = emailTemplate.replace('{{carParkFeeFull}}', (order.carParkFee * nights).toString())
+    emailTemplate = emailTemplate.replace('{{discount}}', room.discount ? (room.discount * 100).toString() : '0')
+    emailTemplate = emailTemplate.replace('{{fullPrice}}', order.fullPrice.toString())
+    emailTemplate = emailTemplate.replace('{{fromDate}}', order.fromDate.toISOString().split('T')[0])
+    emailTemplate = emailTemplate.replace('{{toDate}}', order.toDate.toISOString().split('T')[0])
+    emailTemplate = emailTemplate.replace('{{adults}}', order.adults.toString())
+    emailTemplate = emailTemplate.replace('{{children}}', order.children.toString())
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Cheetah Booking - Invoice',
+      html: emailTemplate,
+      attachments: [
+        {
+          filename: "cheetahbooking-high-resolution-logo.png",
+          path: logoPath,
+          cid: 'logoID'
+        },
+      ],
+    }
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('Email sent: ' + info.response)
+      }
+    })
+    const pdf = new jsPDf()
+    pdf.text(`Invoice for order ${orderId}`, 10, 10)
+    pdf.text(`Name: ${invoice.name}`, 10, 20)
+    pdf.text(`Email: ${invoice.email}`, 10, 30)
+    pdf.text(`Address: ${invoice.address}`, 10, 40)
+    pdf.text(`City: ${invoice.city}`, 10, 50)
+    pdf.text(`Postal code: ${invoice.postalCode}`, 10, 60)
+    pdf.text(`Country: ${invoice.country}`, 10, 70)
+    pdf.text(`Order ID: ${orderId}`, 10, 80)
+    pdf.text(`Hotel name: ${hotel.name}`, 10, 90)
+    pdf.text(`Hotel address: ${hotel.address}`, 10, 100)
+    pdf.text(`Hotel city: ${hotel.city}`, 10, 110)
+    pdf.text(`Hotel country: ${hotel.country}`, 10, 120)
+    pdf.text(`Room: ${room.roomNumber}`, 10, 130)
+    pdf.text(`Adults: ${order.adults}`, 10, 140)
+    pdf.text(`Price for adult: ${room.priceForPerson}PLN`, 10, 150)
+    pdf.text(`Adult price: ${order.adults * room.priceForPerson}PLN`, 10, 160)
+    pdf.text(`Children: ${order.children}`, 10, 170)
+    pdf.text(`Price for children: ${room.childrenPrice}PLN`, 10, 180)
+    pdf.text(`Children price: ${order.children * room.childrenPrice}PLN`, 10, 190)
+    pdf.text(`Nights: ${nights}`, 10, 200)
+    pdf.text(`Car park fee: ${order.carParkFee}`, 10, 210)
+    pdf.text(`Full parking fee: ${order.carParkFee * nights}PLN`, 10, 220)
+    pdf.text(`Discount: ${room.discount ? `${room.discount*100}%` : '0'}`, 10, 230)
+    pdf.text(`Full price: ${order.fullPrice}PLN`, 10, 240)
+    pdf.text(`From: ${order.fromDate.toISOString().split('T')[0]}`, 10, 250)
+    pdf.text(`To: ${order.toDate.toISOString().split('T')[0]}`, 10, 260)
+    pdf.text(`Adults: ${order.adults}`, 10, 270)
+    pdf.text(`Children: ${order.children}`, 10, 280)
+    const pdfPath = path.join(__dirname, '..', '..', '..', 'client', 'public', 'invoice', `invoice_${orderId}.pdf`)
+    pdf.save(pdfPath)
+    return 'Invoice sent'
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 }
 
-export default sendInvoice;
+export default sendInvoice
