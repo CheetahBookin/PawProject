8b01516f9f89c5f3ee98d2c64c77579ed3f3d278
SzKolodziej <szymonkolo555@gmail.com>
2024-04-07 16:43:40 +0200

    added update route to ratingRoute

--- server/src/routes/ratingRoute.ts
+++ server/src/routes/ratingRoute.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { getRatings, postRating } from "../controllers/rating-controller";
+import {getRatings, postRating, updateRating} from "../controllers/rating-controller";
 
 const router: Router = express.Router();
 
 router.get("/", getRatings);
 router.post("/", postRating);
+router.post("/update", updateRating)
 
 export default router;
