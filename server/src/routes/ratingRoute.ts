b7d55dd6566d975bf15dee918d9a97949affc076
SzKolodziej <szymonkolo555@gmail.com>
2024-04-15 23:25:15 +0200

    updated routing

--- server/src/routes/ratingRoute.ts
+++ server/src/routes/ratingRoute.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import {getRatings, postRating, updateRating} from "../controllers/rating-controller";
+import {deleteRating, existingRating, getRatings, postRating} from "../controllers/rating-controller";
 
 const router: Router = express.Router();
 
 router.post("/", getRatings);
-router.post("/post", postRating);
-router.post("/update", updateRating)
+router.post("/post", postRating)
+router.post("/existingRating", existingRating)
+router.delete("/deleteRating", deleteRating)
 
 export default router;
