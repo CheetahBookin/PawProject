789cb61f65d71b83a5f9b9677d00d5fa505656de
SzKolodziej <szymonkolo555@gmail.com>
2024-04-15 23:26:29 +0200

    added deleteRating and existingRating

--- client/src/services/ratingService.ts
+++ client/src/services/ratingService.ts
@@ -19,4 +19,21 @@ const postRates = async(rating: number, comment: string, userId: number, hotelId
     }
 }
 
-export {getRates, postRates}
+const existingRating = async(userId: number, hotelId: number) =>{
+    try {
+        const response = await axios.post('http://localhost:5000/rating/existingRating', {userId, hotelId})
+        return response.data
+    }catch (err){
+        console.log(err)
+    }
+}
+
+const deleteRating = async(userId: number, hotelId: number)=>{
+    try{
+        await axios.delete('http://localhost:5000/rating/deleteRating', {data: {userId, hotelId}},)
+    }catch (err){
+        console.log(err)
+    }
+}
+
+export {getRates, postRates, existingRating, deleteRating}
