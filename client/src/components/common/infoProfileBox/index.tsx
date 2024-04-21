6bf172a6a60c3457a8221221e812e91bb3eed80c
JakubBilski1 <jakub.bilski@spoko.pl>
2024-04-22 01:21:28 +0200

    just some cosmetics

--- client/src/components/common/infoProfileBox/index.tsx
+++ client/src/components/common/infoProfileBox/index.tsx
@@ -1,7 +1,3 @@
-import { Url } from 'next/dist/shared/lib/router/router';
-import Link from 'next/link';
-import React from 'react';
-
 type InfoProfileBoxProps = {
     text: string;
     children: React.ReactNode;
@@ -10,7 +6,7 @@ type InfoProfileBoxProps = {
 function InfoProfileBox({ text, children }: InfoProfileBoxProps) {
   return (
     <div className="bg-white rounded-lg shadow-lg p-4 text-center h-1/3 w-2/5">
-        <h1>{text}</h1>
+        <h1 className="text-xl font-bold mb-4">{text}</h1>
         {children}
     </div>
   );
--- client/src/components/common/userBlocks/index.tsx
+++ client/src/components/common/userBlocks/index.tsx
@@ -5,7 +5,7 @@ type UserBlocksProps = {
 
 function UserBlocks({ header, children }: UserBlocksProps) {
   return (
-    <div className="w-[30%] shadow border rounded-lg flex flex-col justify-center">
+    <div className="w-1/3 shadow border rounded-lg flex flex-col justify-center">
         <h1 className='text-center text-xl font-bold'>{header}</h1>
         {children}
     </div>
