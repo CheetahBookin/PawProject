c33faa5c8662fdac2d70b1506b57e98dc6bfd04e
AKopydlowski <olekkopy@gmail.com>
2024-05-15 09:59:20 +0200

    Add test for bargainsGallery (not working yet)

---
+++ tests/bargainsGallery.spec.ts
@@ -0,0 +1,30 @@
+//Test not working
+
+import { test, expect } from '@playwright/test';
+
+test('Renders correctly', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  const bargainsGallery = await page.waitForSelector('.w-[65%]');
+  expect(bargainsGallery).toBeTruthy();
+});
+
+test('Navigates to correct page on item click', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await page.click('.bg-brand-secondary');
+  await page.waitForNavigation();
+  expect(page.url()).toContain('/hotel/');
+});
+
+// test('Calculates discounted price correctly', async ({ page }) => {
+//   await page.goto('http://localhost:3000');
+//   const oldPrice = 100;
+//   const discount = 0.2;
+//   const expectedPrice = oldPrice - (oldPrice * discount);
+
+//   await page.waitForSelector('.bg-brand-secondary p:last-child');
+//   const newPriceTextElement = await page.$('.bg-brand-secondary p:last-child');
+//   const newPriceText = await (newPriceTextElement ? newPriceTextElement.textContent() : null);
+//   const newPrice = +newPriceText?.replace('New price: ', '').replace('zł', '') || 0;
+
+//   expect(newPrice).toBe(expectedPrice);
+// });
