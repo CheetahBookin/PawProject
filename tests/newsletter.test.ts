a4f2303f7c13515d5f6364399141a00e3beb79b5
AKopydlowski <olekkopy@gmail.com>
2024-05-15 13:07:58 +0200

    Add test newsletter (not working)

---
+++ tests/newsletter.test.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('successful newsletter subscription', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    try {
+        // waiting for locator('input[type="email"]') to be visible
+        await page.waitForSelector('.mx-auto input[type="email"]', { timeout: 10000 });
+    } catch (error) {
+        console.error("Failed to locate the email input field:", error);
+        throw error;
+    }
+
+    await page.fill('input[type="email"]', 'test@example.com');
+    await page.click('button[type="submit"]');
+    await page.waitForSelector('.text-green-500');
+    const successMessage = await page.textContent('.text-green-500');
+
+    expect(successMessage).toBe('Thank you for subscribing!');
+});
