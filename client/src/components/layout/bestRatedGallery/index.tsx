58142c2a9235c62bf791820f7ac4d44276d368b3
JakubBilski1 <jakub.bilski@spoko.pl>
2024-05-27 21:10:07 +0200

    fixed some styling

--- client/src/components/layout/bestRatedGallery/index.tsx
+++ client/src/components/layout/bestRatedGallery/index.tsx
@@ -32,29 +32,30 @@ const BesRatedGallery = ()=>{
     }
 
     return(
-        <>
+        <div className="w-[65%] flex flex-col items-center gap-4 pb-8">
             <h1 className="font-bold text-3xl">Best rated</h1>
-            <Carousel className="w-[65%]">
+            <Carousel className="w-full">
                 <CarouselContent className="-ml-1">
                     {data?.map((item: BestRated, index: number) => (
-                        <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                            <div className="h-full bg-brand-primary rounded-2xl">
-                                <img src={item.image} alt={item.name} className="w-full h-1/2 rounded-t-2xl"/>
-                                <div className='w-full flex flex-col items-center justify-around h-1/2'>
-                                    <p className='text-black'>{item.name}, {item.country}</p>
-                                    <div className="bg-brand-secondary p-8 rounded-2xl cursor-pointer hover:bg-gray-300 transition-all" onClick={()=>handleClick(item.name, item.id)}>
-                                        <p>Average rate: {item.avg_rate ? item.avg_rate : 'Not rated'}</p>
-                                        <p>Number of rates: {item.rates_count}</p>
-                                    </div>
+                        <CarouselItem key={index} className="basis-1/3 h-full md:w-1/2 lg:w-1/3">
+                        <div className="h-full bg-brand-primary rounded-2xl">
+                            <img src={item.image} alt={item.name} className="w-full h-56 object-cover rounded-t-3xl" />
+                            <div className='p-6'>
+                                <p className='text-gray-800 text-lg font-semibold'>{item.name}, {item.country}</p>
+                                <div className="mt-4">
+                                    <p className="text-gray-600">Average Rate: {item.avg_rate ? item.avg_rate : 'Not Rated'}</p>
+                                    <p className="text-gray-600">Number of Rates: {item.rates_count}</p>
                                 </div>
+                                <button onClick={() => handleClick(item.name, item.id)} className="mt-4 w-full py-2 px-4 bg-blue-500 text-white rounded-full focus:outline-none hover:bg-blue-600 transition-all">View Details</button>
                             </div>
-                        </CarouselItem>
+                        </div>
+                    </CarouselItem>
                     ))}
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
             </Carousel>
-        </>
+        </div>
     )
 }
 
