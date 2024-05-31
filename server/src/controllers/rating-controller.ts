7d6314b3df80e2e465e5c78224b0202f7b2883f6
SzKolodziej <szymonkolo555@gmail.com>
2024-06-01 01:40:19 +0200

    rating-controller updated

--- server/src/controllers/rating-controller.ts
+++ server/src/controllers/rating-controller.ts
@@ -12,19 +12,22 @@ interface Rating extends Request {
 }
 
 const getRatings = async (req: Rating, res: Response) => {
-    const {hotelId} = req.body
-  const ratings = await prisma.rates.findMany({
-      where: {hotelId: hotelId},
-      select: {
-          rate: true,
-          message: true,
-          userId: true,
-          hotelId: true,
-          user: {select: {username: true}}
-      }
-  }
-  );
-  res.json(ratings);
+    try {
+        const {hotelId} = req.body
+        const ratings = await prisma.rates.findMany({
+                where: {hotelId: hotelId},
+                select: {
+                    rate: true,
+                    message: true,
+                    userId: true,
+                    hotelId: true,
+                    user: {select: {username: true}}
+                }
+            })
+        res.status(200).json(ratings)
+    }catch (err){
+        res.status(500).json({ error: "Can't get ratings" })
+    }
 }
 
 const getUsersRatings = async (req: Rating, res: Response) => {
@@ -45,69 +48,80 @@ const getUsersRatings = async (req: Rating, res: Response) => {
 }
 
 const postRating = async (req: Rating, res: Response) =>{
-    const {rating, comment, userId, hotelId} = req.body;
+    try {
+        const {rating, comment, userId, hotelId} = req.body;
 
-    const existingRating = await prisma.rates.findFirst({
-        where: {
-            userId: userId,
-            hotelId: hotelId
-        }
-    })
-
-    if(existingRating) {
-        const updatedRating = await prisma.rates.update({
+        const existingRating = await prisma.rates.findFirst({
             where: {
-                id: existingRating.id
-            },
-            data: {
-                rate: rating,
-                message: comment
+                userId: userId,
+                hotelId: hotelId
             }
         })
 
-        res.json(updatedRating)
-    }
-    else{
-        const newRating = await prisma.rates.create({
-            data: {
-                hotelId: hotelId,
-                userId: userId,
-                rate: rating,
-                message: comment,
-                date: new Date()
-            }
-        });
-        res.json(newRating);
+        if (existingRating) {
+            const updatedRating = await prisma.rates.update({
+                where: {
+                    id: existingRating.id
+                },
+                data: {
+                    rate: rating,
+                    message: comment
+                }
+            })
+
+            res.status(201).json(updatedRating)
+        } else {
+            const newRating = await prisma.rates.create({
+                data: {
+                    hotelId: hotelId,
+                    userId: userId,
+                    rate: rating,
+                    message: comment,
+                    date: new Date()
+                }
+            });
+            res.status(201).json(newRating);
+        }
+    }catch (err){
+        res.status(500).json({ error: "Can't post rating" })
     }
 }
 
 const existingRating = async(req: Rating, res: Response)=>{
-    const {userId, hotelId} = req.body
+    try {
+        const {userId, hotelId} = req.body
 
-    const _existingRating = await prisma.rates.findFirst({
-        where:{
-            userId: userId,
-            hotelId: hotelId
+        const _existingRating = await prisma.rates.findFirst({
+            where: {
+                userId: userId,
+                hotelId: hotelId
+            }
+        })
+
+        if (_existingRating) {
+            res.send(true)
+        } else {
+            res.send(false)
         }
-    })
-
-    if(_existingRating){
-        res.send(true)
-    }
-    else{
-        res.send(false)
+    }catch (err){
+        res.status(500).json({ error: "Internal server error" })
     }
 }
 
 const deleteRating = async(req: Rating, res: Response)=>{
-    const {userId, hotelId} = req.body
+    try {
+        const {userId, hotelId} = req.body
 
-    await prisma.rates.deleteMany({
-        where:{
-            userId: userId,
-            hotelId: hotelId
-        }
-    })
+        await prisma.rates.deleteMany({
+            where: {
+                userId: userId,
+                hotelId: hotelId
+            }
+        })
+        res.status(200).json({message: "Rating deleted"})
+    }catch (err){
+        res.status(500).json({ error: "Can't delete rating" })
+    }
 }
 
 export { getRatings, postRating, existingRating, deleteRating, getUsersRatings}
